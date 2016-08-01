3d7e00ca80f5ecdbb721b0cab54fd9381d59ec6a
Sivard <sekiranositel@mail.ru>
2016-08-01 14:23:28 +0300

    hotfix. fix seed.rb create cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ doc = Nokogiri::HTML(open('https://www.learnathome.ru/blog/100-beautiful-words')
 
 doc.search('//table/tbody/tr').each do |row|
   original = row.search('td[2]')[0].content.downcase
-  translated = row.search('td[1]')[0].content.downcase
+  translated = row.search('td[4]')[0].content.downcase
   card = block.cards.build(original_text: original, translated_text: translated, user_id: user.id)
+  card.save
 end
