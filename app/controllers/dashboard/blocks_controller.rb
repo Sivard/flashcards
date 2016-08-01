51b843480bd72cee18caad676e07e921f4ef5b5a
Sivard <sekiranositel@mail.ru>
2016-08-01 17:23:00 +0300

    hotfix. add respond_to to blocks_controller and cards_controller

--- app/controllers/dashboard/blocks_controller.rb
+++ app/controllers/dashboard/blocks_controller.rb
@@ -1,6 +1,7 @@
 class Dashboard::BlocksController < Dashboard::BaseController
   before_action :set_block, only: [:destroy, :edit, :update, :set_as_current,
                                    :reset_as_current]
+  respond_to :html
 
   def index
     @blocks = current_user.blocks.all.order('title')
