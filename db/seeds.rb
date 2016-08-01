eaabe4e0e078e07d2bfed454ae43068c62225bfd
Sivard <sekiranositel@mail.ru>
2016-08-01 18:56:30 +0300

    refactoring. seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,12 @@ require 'nokogiri'
 require 'open-uri'
 
 user = User.create(email: 'email@example.ru', password: '1234', password_confirmation: '1234')
-block = user.blocks.build(title: 'Карточка №1')
-block.save
+block = user.blocks.create(title: 'Карточка №1')
 
 doc = Nokogiri::HTML(open('https://www.learnathome.ru/blog/100-beautiful-words'))
 
 doc.search('//table/tbody/tr').each do |row|
   original = row.search('td[2]')[0].content.downcase
   translated = row.search('td[4]')[0].content.downcase
-  card = block.cards.build(original_text: original, translated_text: translated, user_id: user.id)
-  card.save
+  block.cards.create(original_text: original, translated_text: translated, user_id: user.id)
 end
