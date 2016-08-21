e097b398a611db69ae492d0a7ffa36742e60b6b9
Sivard <sekiranositel@mail.ru>
2016-08-21 15:51:24 +0300

    test. add capybara and sorcery congig

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,4 +48,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.include Capybara::DSL
+  config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
+  config.include Sorcery::TestHelpers::Rails::Integration, type: :feature
 end
