62fff8bfe195bac50d7c17edbf0a09348c64e002
Sivard <sekiranositel@mail.ru>
2016-08-25 10:04:06 +0300

    hotfix. fix impressionist flickr action

--- app/controllers/dashboard/flickrs_controller.rb
+++ app/controllers/dashboard/flickrs_controller.rb
@@ -1,9 +1,8 @@
 class Dashboard::FlickrsController < ApplicationController
-  impressionist
-
   def index
     text = params[:text]
     flickr = Flickr.new(text).get_list
+    impressionist(Card.find(params[:card_id]), "get list from flickr api")
 
     render json: flickr.to_json
   end
